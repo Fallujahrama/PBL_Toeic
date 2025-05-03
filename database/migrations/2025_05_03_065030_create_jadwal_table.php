7493293225c3c8125c616723886bc6171c055950
Fallujahrama <fallujahrc@gmail.com>
2025-05-03 14:58:00 +0700

    migration and seeder beberapa tabel

---
+++ database/migrations/2025_05_03_065030_create_jadwal_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jadwal', function (Blueprint $table) {
+            $table->id('jadwal_id');
+            $table->date('tanggal');
+            $table->string('informasi');
+            $table->string('file_info');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jadwal');
+    }
+};
